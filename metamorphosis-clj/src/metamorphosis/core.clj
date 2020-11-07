3fd2906ba7ecf89a5ac4e106623ca237301ffc34
Seb CTech <sebastian.w94@hotmail.com>
2020-11-07 14:04:34 +0100

    Clojure Initialization

---
+++ metamorphosis-clj/src/metamorphosis/core.clj
@@ -0,0 +1,7 @@
+(ns metamorphosis.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ metamorphosis-clj/test/metamorphosis/core_test.clj
@@ -0,0 +1,7 @@
+(ns metamorphosis.core-test
+  (:require [clojure.test :refer :all]
+            [metamorphosis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
