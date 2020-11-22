b89544bda722b0d0d8097097e1faad266c56f3d5
Seb CTech <sebastian.w94@hotmail.com>
2020-11-22 22:40:22 +0100

    Divided program into modules

---
+++ src/metamorphosis/l_system.clj
@@ -0,0 +1,7 @@
+(ns metamorphosis.l-system)
+
+;TODO: Define Alphabet
+
+;TODO: Find visual translation made out of composable functions that is fitting for a beginning motif. Ideally 3D
+(def axiom [:a :b :c :d])
+
