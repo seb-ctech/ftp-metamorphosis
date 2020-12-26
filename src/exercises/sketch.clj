1d88d87d676379024b968acf5e88ff6aec42860a
seb <sebastian.w94@hotmail.com>
2020-12-26 11:18:39 +0100

    Added random sequence generator

--- src/exercises/sketch.clj
+++ src/exercises/sketch.clj
@@ -4,22 +4,26 @@
 
 (def number-of-cells 5)
 
-(defn generate-grid []
+; Using lazy sequences to generate an infinite range of random numbers, that always stay the same
+(defn generate-random-sequence []
+    (map #(+ (Math/random) (- % %) (range))))
+
+(defn generate-grid [rand]
     (let [cell-size (/ (q/width) number-of-cells)]
-        (loop [cells [] x 0 y 0]
+        (loop [cells [] x 0 y 0 i 0]
             (let [next-x (+ x cell-size)
                   next-y (+ y cell-size)
                   top-left {:x x :y y}
                   top-right {:x next-x :y y}
                   bottom-left {:x x :y next-y}
                   bottom-right {:x next-x :y next-y}
-                  flipped (< (Math/random) 0.5)
+                  flipped (< (get rand i) 0.5)
                   cell {:top-left top-left :top-right top-right :bottom-left bottom-left :bottom-right bottom-right :flipped? flipped}
                   new-cells (conj cells cell)]
                 (if (< next-x (q/width))
-                    (recur new-cells next-x y)
+                    (recur new-cells next-x y (inc i))
                     (if (< next-y (q/height))
-                        (recur new-cells 0 next-y)
+                        (recur new-cells 0 next-y (inc i))
                         new-cells))))))
 
 (defn draw-diagonal [cell]
@@ -32,7 +36,9 @@
     (draw-diagonal cell))
 
 (defn setup []
-    {:cells (generate-grid)})
+    (let [rand (generate-random-sequence)]
+    {   :random-seq rand
+        :cells (generate-grid rand)}))
 
 (defn draw-state [state]
     (q/background 255)
@@ -40,4 +46,4 @@
         (draw-cell cell)))
 
 (defn mouse-clicked [state]
-    {:cells (generate-grid)})
+    (assoc state :cells (generate-grid (:random-seq state))))
