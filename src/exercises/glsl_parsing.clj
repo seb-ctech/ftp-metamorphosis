b2a47df3c9ec5de9b0cc5f9224afb79f19c44e31
seb <sebastian.w94@hotmail.com>
2020-12-27 15:06:27 +0100

    Exercise 3: made first simple composed parsing system

--- src/exercises/glsl_parsing.clj
+++ src/exercises/glsl_parsing.clj
@@ -4,6 +4,25 @@
 
 (defn setup [])
 
-(defn update [])
+(def return-break "\n\n")
 
-(defn draw [])
+(defn glsl-prefix []
+    (str "#ifdef GL_ES\n" 
+         "precision mediump float;\n" 
+         "#endif" return-break 
+         "uniform vec2 u_resolution;\n" 
+         "uniform float u_time;" return-break))
+
+(defn glsl-functions []
+    (str ""))
+
+(defn glsl-out [v]
+   (str "gl_FragColor = vec4(" v ");"))
+
+(defn glsl-main []
+    (str "void main(){" return-break (glsl-out "1, 0, 1, 1") return-break "}"))
+
+(defn assemble-glsl []
+    (str (glsl-prefix)(glsl-functions)(glsl-main)))
+
+(spit "src/exercises/glsl/test.frag" (assemble-glsl))
