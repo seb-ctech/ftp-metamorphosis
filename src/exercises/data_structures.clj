8c1550ddfbbd0a930e7d894762c08b01cb27410f
seb <sebastian.w94@hotmail.com>
2020-12-28 16:42:30 +0100

    Started exercise 4 with the vampire-db example of Clojure for the Brave

--- src/exercises/data_structures.clj
+++ src/exercises/data_structures.clj
@@ -1,2 +1,34 @@
 ; 4. Exercise is about building complex data-structures and applying algorithms that do useful conceptuals things with it,
 ; with the built-in clojure functions, map, reduce, etc...
+(ns exercises.data-structures)
+
+(def filename "src/exercises/csv/suspects.csv")
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int [str]
+    (Integer. str))
+
+(def conversions {:name identity :glitter-index str->int})
+
+(defn conversion [vamp-key value]
+    ((get conversions vamp-key) value))
+
+(defn parse
+    "Convert a CSV into rows of columns"
+    [string]
+    (map #(clojure.string/split % #",")
+        (clojure.string/split string #"\n")))
+
+(defn mapify
+    "Return a sequence of Maps :name ... :index ..."
+    [rows]
+    (map (fn [unmapped-row]
+            (reduce (fn [row-map [vamp-key value]]
+                        (assoc row-map vamp-key (convert vamp-key value)))
+                {}
+                (map vector vamp-keys unmapped-row)))
+        rows))
+
+(defn glitter-filter
+    [minimum-glitter records]
+    (filter #(>= (:glitter-index %) minimum-glitter) records))
