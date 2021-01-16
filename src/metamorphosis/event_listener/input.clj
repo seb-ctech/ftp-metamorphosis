f4e41105032c52f33f955484b03e63b27aafd7a0
Seb - The Layered Mind <sebastian.w94@hotmail.com>
2021-01-16 16:55:53 +0100

    designed first iteration for abstract data-structures

--- src/metamorphosis/event_listener/input.clj
+++ src/metamorphosis/event_listener/input.clj
@@ -2,6 +2,19 @@
 
 ;;=== PROTOTYPE OF ABSTRACT INPUT DATA-STRUCTURE ===
 
+; What about forms at the same time?
+; Inputs are made out of signals, that have an intensity and a duration.
+; A Signal represent a discrete input form that can be distinguished from others
+
+
+(def test-input [
+    {:signal :A :intensity 0.1 :duration 1/10}
+    {:signal :B :intensity 0.1 :duration 3/10}
+    {:signal :break :duration 2/10}
+    {:signal :B :intensity 0.1 :duration 3/10}
+])
+
+
 (def input-interval 2000)
 (def a-input "a")
 
--- src/metamorphosis/meta_ruleset/formal_system.clj
+++ src/metamorphosis/meta_ruleset/formal_system.clj
@@ -3,7 +3,30 @@
 ;TODO: Define Alphabet as abstract structure that can be used to define mutating rules and graphic commands
 ;TODO: Implement function to create axiom from input-data-structure provided by the "event-listener" module
 
-(def alphabet ["A" "B" "C"])
+;=== PROTOTYPE OF AN ABSTRACT DATA STRUCTURE ===
+
+; One generation contains the generation count for the scope a sequence of objects that define a class,
+; that has a different meaning by the translation context it is used in and an index for the list of 
+; commands of the same class. Classes can be of type: body, transform, property, amount.
+
+; BODY: Draws some form of geometry; builds some sequence
+; TRANSFORM: Changes the relation between Bodies in some form
+; PROPERTY: Some property that the next Bodies will have
+; AMOUNT: Some amount that either the next Body, Transform or Property will have. Default is "0"
+
+(def example-theorem {:gen 0 
+                      :sequence [{:class :transform :index 1}
+                                 {:class :body :index 2}
+                                 {:class :amount :index 1}
+                                 {:class :transform :index 1}
+                                 {:class :property :index 2}
+                                 {:class :property :index 0}
+                                 {:class :amount :index 2}]})
+
+
+; The alphabet is made out of the different classes that commands can have
+
+(def alphabet [:body :transform :property :amount])
 
 (defn random-alphabet-letter []
     (get alphabet (rand-int (count alphabet))))
